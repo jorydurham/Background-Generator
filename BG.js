31ce2498144d99eccd98d3170bf0ad5b180abc85
jorydurham <118610695+jorydurham@users.noreply.github.com>
2022-12-05 12:02:47 +0900

    Add files via upload

---
+++ BG.js
@@ -0,0 +1,25 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+css.textContent = body.style.background + 
+"linear-gradient(to right, rgb(0, 255, 0), rgb(255, 0, 0));";
+
+function colorChange() {
+	body.style.background="linear-gradient(to right," 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+
+
+
+color1.addEventListener("input", colorChange);
+
+color2.addEventListener("input", colorChange);
+
